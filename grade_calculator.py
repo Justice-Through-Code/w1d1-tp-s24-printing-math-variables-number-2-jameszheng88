20d436827333723b9fb062f41eebb59a2341189c
James Zheng <jameszheng88@gmail.com>
2024-05-13 21:58:32 -0400

    updated grade_calculator for user and grade input and calculate grade average

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,15 +1,18 @@
+# This script calculates the average grade of math, science, and English scores
+# Author: James Zheng
+
 def calculate_average_grade():
     # Prompt the user for their name and store it in the student_name variable
-    student_name = ""
+    student_name = input("Please enter your name ")
 
     # Prompt the user for their scores in Math, Science, and English
     # Store the scores in the respective variables: math_score, science_score, english_score
-    math_score = 0
-    science_score = 0
-    english_score = 0
+    math_score = int(input("Please enter your math score "))
+    science_score = int(input("Please enter your science score "))
+    english_score = int(input("Please enter your english score "))
 
     # Calculate the average grade
-    average_grade = 0
+    average_grade = (math_score + science_score + english_score)/3
 
     # Return the student's name and their average grade
     return student_name, average_grade
@@ -19,4 +22,4 @@ if __name__ == '__main__':
     student_name, average_grade = calculate_average_grade()
 
     # Print the student's name and their average grade
-    print(f"")
+    print(f"{student_name}'s average grade for math, science, and english class is {average_grade}.@")
